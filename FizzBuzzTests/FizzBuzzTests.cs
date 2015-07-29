6f0c1625d35d6fc05ae873ab3f8cf0e2ccf62564
MrAlexandre76 <alex10_76@hotmail.fr>
2015-07-29 09:33:50 +0200

    FizzBuzzTests
    Classe de test du projet FizzBuzz

---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void TestUnNombreEnvoyeDivisibleParTrois_LaValeurRetourneeEstFizz()
+        {
+            var fizzBuzz = new FizzBuzz();
+            Check.That(fizzBuzz.TesterNombre(3)).IsEqualTo("Fizz");
+        }
+
+        [TestMethod]
+        public void TestUnNombreEnvoyeDivisibleParCinq_LaValeurRetourneeEstBuzz()
+        {
+            var fizzBuzz = new FizzBuzz();
+            Check.That(fizzBuzz.TesterNombre(5)).IsEqualTo("Buzz");
+        }
+
+        [TestMethod]
+        public void TestUnNombreEnvoyeDivisibleParTroisEtParCinq_LaValeurRetourneeEstFizzBuzz()
+        {
+            var fizzBuzz = new FizzBuzz();
+            Check.That(fizzBuzz.TesterNombre(15)).IsEqualTo("FizzBuzz");
+        }
+
+        [TestMethod]
+        public void TestUnNombreEnvoyeDivisibleNiParTroisNiParCinq_LaValeurRetourneeEstVide()
+        {
+            var fizzBuzz = new FizzBuzz();
+            Check.That(fizzBuzz.TesterNombre(8)).IsEqualTo(string.Empty);
+        }
+    }
+}
